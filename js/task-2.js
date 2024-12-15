df262f1c5b88a0ac8d8b734d0f1fc4e85f8c59b1
Iryna Filatova <iryna.filatova@gmail.com>
2024-12-15 12:20:26 +0100

    Initial commit

---
+++ js/task-2.js
@@ -0,0 +1,14 @@
+function makeArray(firstArray, secondArray, maxLength) {
+  const modifiedArray = firstArray.concat(secondArray);
+  if (modifiedArray.length > maxLength) {
+    return modifiedArray.slice(0, maxLength);
+  } else {
+    return modifiedArray;
+  }
+}
+console.log(makeArray(["Mango", "Poly"], ["Ajax", "Chelsea"], 3)); // ["Mango", "Poly", "Ajax"]
+console.log(makeArray(["Mango", "Poly", "Houston"], ["Ajax", "Chelsea"], 4)); // ["Mango", "Poly", "Houston", "Ajax"]
+console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 3)); // ["Mango", "Ajax", "Chelsea"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 2)); // ["Earth", "Jupiter"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus"], 4)); // ["Earth", "Jupiter", "Neptune", "Uranus"]
+console.log(makeArray(["Earth", "Jupiter"], ["Neptune", "Uranus", "Venus"], 0)); // []
